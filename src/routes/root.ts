244336eed1c28b46900e9996eabceaf817e2358f
Emika <itallomkspop@gmail.com>
2024-02-14 22:50:38 -0300

    define an route to test happy_msg decorator

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,18 +1,16 @@
 import { FastifyPluginAsync } from "fastify";
 import { PeopleSchema } from "../types/People";
-import z, { ZodError } from "zod";
-import { fromZodError } from "zod-validation-error";
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get("/hello", async (request, reply) => {
+  fastify.get("/hello", async function (req, reply) {
     return { message: "hello" };
   });
 
-  fastify.get("/world", async function (request, reply) {
+  fastify.get("/world", async function (req, reply) {
     return { message: "world" };
   });
 
-  fastify.post("/people", async (req, reply) => {
+  fastify.post("/people", async function (req, reply) {
     const body = PeopleSchema.safeParse(req.body);
 
     if (!body.success) {
@@ -22,6 +20,21 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     }
 
     return body.data;
+  });
+
+  fastify.post("/decorate", async function (req, reply) {
+    const body = PeopleSchema.safeParse(req.body);
+
+    if (!body.success) {
+      reply.code(422);
+
+      return body.error.format();
+    }
+
+    return {
+      hello: `${this.happy_msg.hello} ${body.data.name}`,
+      bye: `${this.happy_msg.bye} ${body.data.name}`,
+    };
   });
 };
 
